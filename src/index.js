3b3fe3ba5807e62e349df982944f6e5ce470baba
Michael Lee <mike.sj.lee@gmail.com>
2018-02-12 17:26:03 -0500

    no longer needed

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import { render } from "react-dom";
 import { createStore } from "redux";
 import Root from "./containers/Root";
 import rootReducer from "./reducers";
-import registerServiceWorker from "./registerServiceWorker";
 
 const store = createStore(rootReducer);
 
 render(<Root store={store} />, document.getElementById("root"));
-registerServiceWorker();
